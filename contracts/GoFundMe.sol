8dcf41e12b61aee90ccac2255743fb4dd0ee7197
jiokeokwuosa <okwuosachijioke@gmail.com>
2023-02-16 17:06:29 +0100

    updated readme...

--- contracts/GoFundMe.sol
+++ contracts/GoFundMe.sol
@@ -2,19 +2,6 @@
 pragma solidity ^0.8.7;
 import "./PriceConverterLibrary.sol";
 
-/*
- in this contract we want to
-  1. Get funds from users
-  2. Withdraw funds
-  3.Set a minimum funding value in USD
-
-  Each transaction contains information like nonce(transaction count for the account), gas price,
-  gas limit, to(address that the transaction is sent to), value(amount of wei to send), data(what to send
-  to `to` address), vrs(crypotographic magic) component of the transaction signature.
-
-  Note: you can import console.log package and work with it in your contract
-*/
-// you can have different custom errors for different cases
 error GoFundMe__NotOwner(string message);
 error GoFundMe__AmountNotEnough(string message);
 error GoFundMe__WithdrawalFailed(string message);
@@ -27,25 +14,6 @@ error GoFundMe__WithdrawalFailed(string message);
  */
 contract GoFundMe {
   using PriceConverter for uint256;
-  /* this line above will make the functions of price converter library imported in this contract 
-    accessible via uint256 eg msg.value.getConversionRate(), remember that msg.value has type uint256
-    if the library function takes parameters, the uint256 value is regarded as the the first paramter
-    eg msg.value in the example msg.value.getConversionRate(), the other parameters of the function
-    can then be passed in the parenthesis of the function
-
-    constants and immutable are used to declare variables that won't change and it is more gas efficient,
-    because they are stored into the bytecode and not on the storage slot, they are easier to be read.
-    constants should be declared with capital letters and underscore is used to join mutiple words
-    use immutable for variables that will only recieve value once but in a different place from the line
-    where it was declared eg variables that recieve there values in a contructor, immutable variables names
-    can be preceded with i_ eg i_owner.
-    using less require statement can  be gas efficient because the second argument of the require statement
-    which is the string to be displayed when error occurs will need to be stored and thus will consume more gas
-    rather work with custom errors and if statements which is more gas efficient.
-    storage variables should be preceded with s_
-    reading and writing to storage consumes much gas
-    */
-
   uint256 public constant MINIMUM_USD = 5 * 1e18; // we mutiply by 18 zeros so the unit will be same as etherium
   address[] public s_funders;
   mapping(address => uint256) public s_addressToAmountFunded;
@@ -72,27 +40,13 @@ contract GoFundMe {
 
   fallback() external payable {
     fund();
-  }
-
-  /* In order to send etherium you have to add payable keyword to the processing function
-   * msg.value returns the value of wei a person is sending
-   * msg.sender returns the address of whoever calls the function
-   */
+  }  
 
   /**
    * @notice This funtion handles the funding of ether
    * if a function has params you can document them with the params keyword  
    */
-  function fund() public payable {
-    /* to validate the amount of ether the user is sending
-          the require function checks whether the condition is passed, if condition fails
-          it will display the message in the second argument, revert the previous action done
-          in that function call (if any) and return the remaining gas fee (so the gas fee used to do
-          any action before the line of code with the require statment will be lost) eg
-           require(msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,"Amount sent is not enough");
-           but custom errors are more gas efficient than require
-        */
-
+  function fund() public payable {  
     if (msg.value.getConversionRate(s_priceFeed) < MINIMUM_USD)
       revert GoFundMe__AmountNotEnough({message: "Amount sent is not enough"});
 
@@ -106,19 +60,7 @@ contract GoFundMe {
       s_addressToAmountFunded[funder] = 0;
     }
     //reset s_funders array to new array with zero item
-    s_funders = new address[](0);
-    /*withdraw the actual funds, we can do that through transfer, send or call function,
-        msg.sender is of type address, while payable(msg.sender) is of type payable address,
-        address(this).balance returns the balance of the contract. The transfer and send method
-        consumes 2300 gas and fails if the transaction requires more than 2300 gas
-        */
-    // // using transfer, it automatically reverts if the transfer fails
-    // payable(msg.sender).transfer(address(this).balance);
-    // /*using send, it doen't revert if transfer fails, rather it returns a boolean value false,
-    //  so we might need to use require to revert it ourselves if it fails*/
-    // bool sendSuccess = payable(msg.sender).send(address(this).balance);
-    // require(sendSuccess,"Transfer failed");
-    // // using call, returns 2 values, first one indicates whether it was successful and the other is the returned data(bytes memory dataReturned)
+    s_funders = new address[](0);   
     (bool callSuccess, ) = payable(msg.sender).call{
       value: address(this).balance
     }("");
